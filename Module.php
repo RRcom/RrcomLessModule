d409b376c91e7bd47a49f33a9625edb7f8837a66
Resty Rizal <resty_rizal@live.com>
2014-10-29 11:40:26 +0800

    beta test version

--- Module.php
+++ Module.php
@@ -1,29 +1,7 @@
 <?php
-namespace RrcomLessModule;
-
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+/**
+ * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/RrcomLessModule' in order to respect PSR-0
+ */
+require_once __DIR__ . '/src/RRcomLessModule/Module.php';
---
+++ src/RrcomLessModule/Module.php
@@ -0,0 +1,34 @@
+<?php
+namespace RrcomLessModule;
+
+use Zend\Mvc\MvcEvent;
+use lessc;
+
+class Module
+{
+    protected $config = null;
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $config = $e->getApplication()->getServiceManager()->get('Config');
+        if(empty($config['rrcomlessmodule']) || empty($config['rrcomlessmodule']['enable'])) return;
+        $this->config = $config['rrcomlessmodule'];
+        $this->writeToFile();
+    }
+
+    public function writeToFile()
+    {
+        $lessc = new lessc();
+        if(empty($this->config['files'])) return;
+        foreach($this->config['files'] as $file) {
+            if(!empty($file['less-file']) && is_file($file['less-file'])) {
+                $lessc->compileFile($file['less-file'], $file['css-file']);
+            }
+        }
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php.dist';
+    }
+}
