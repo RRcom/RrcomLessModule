40212c127df0d48c08bec7eebb32510ed97245cf
Resty Rizal <resty_rizal@live.com>
2015-01-20 18:24:25 +0800

    add compress output in config

--- src/RrcomLessCompiler/LessCompiler.php
+++ src/RrcomLessCompiler/LessCompiler.php
@@ -17,6 +17,8 @@ class LessCompiler
         if(empty($config['files'])) return;
         foreach($config['files'] as $file) {
             if(!empty($file['less-file']) && is_file($file['less-file'])) {
+                if(!empty($file['formatter'])) $this->lessc->setFormatter($file['formatter']);
+                else $this->lessc->setFormatter(null);
                 $this->lessc->compileFile($file['less-file'], $file['css-file']);
             }
         }
